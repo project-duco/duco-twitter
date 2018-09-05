90c0330d9d1f49c1526b706b756d1198827ed224
project-duco <ducojl@gmail.com>
2018-09-05 15:45:58 -0700

    formatting
    :q

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,4 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'main#landing'
   post '/tweets', to: 'main#get_25_tweets_by_user'
-
 end
