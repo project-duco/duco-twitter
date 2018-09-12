ae2b3a2ea651684bb305e9843ab0f775662725e0
project-duco <ducojl@gmail.com>
2018-09-12 11:37:02 -0500

    fill out twitter config

--- config/application.rb
+++ config/application.rb
@@ -23,8 +23,10 @@ module DucoTwitter
 
     # initialize twitter client
     CLIENT = Twitter::REST::Client.new do |config|
-      config.consumer_key    = ENV['TWITTER_API_KEY']
-      config.consumer_secret = ENV['TWITTER_API_SECRET']
+      config.consumer_key        = ENV['TWITTER_API_KEY']
+      config.consumer_secret     = ENV['TWITTER_API_SECRET']
+      config.access_token        = ENV['TWITTER_API_ACCESS_TOKEN']
+      config.access_token_secret = ENV['TWITTER_API_ACCESS_TOKEN_SECRET']
     end
 
     # Initialize configuration defaults for originally generated Rails version.
